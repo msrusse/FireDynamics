8515687073cafbb1da4dcb4d938cdf80c232ecec
msrusse <30694747+msrusse@users.noreply.github.com>
2018-02-14 15:13:46 -0600

    Addition of Activity for Flashover calculation. Also added values for the spinners with units and INterior Lining Material

---
+++ app/src/main/java/com/example/masonrussell/firedynamics/FlashOver.java
@@ -0,0 +1,13 @@
+package com.example.masonrussell.firedynamics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlashOver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash_over);
+    }
+}
