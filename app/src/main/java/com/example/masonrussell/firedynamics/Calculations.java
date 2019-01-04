2198fd549780b9b4c575f0c83f9ff495ead2cc7e
Mason S. Russell <mason.russell@okstate.edu>
2019-01-03 20:06:18 -0600

    Starts the UI updates, updating fully the flame height and conduction pages

--- app/src/main/java/com/example/masonrussell/firedynamics/Calculations.java
+++ app/src/main/java/com/example/masonrussell/firedynamics/Calculations.java
@@ -6,42 +6,26 @@ package com.example.masonrussell.firedynamics;
 
 class Calculations
 {
-    static double Calculatehk(double thermalConductivity, double intLiningThickness)
-    {
-        return thermalConductivity/intLiningThickness;
-    }
+    static double Calculatehk(double thermalConductivity, double intLiningThickness) { return thermalConductivity/intLiningThickness; }
 
     static double CalculateAv(double ventWidth, double ventHeight)
     {
         return ventHeight*ventWidth;
     }
 
-    static double CalculateAT(double compWidth, double compLength, double compHeight, double ventWidth, double ventHeight)
-    {
-        return ((2*(compWidth*compLength) + 2*(compHeight*compWidth) + 2*(compHeight*compLength)) - CalculateAv(ventWidth, ventHeight));
-    }
+    static double CalculateAT(double compWidth, double compLength, double compHeight, double ventWidth, double ventHeight) { return ((2*(compWidth*compLength) + 2*(compHeight*compWidth) + 2*(compHeight*compLength)) - CalculateAv(ventWidth, ventHeight)); }
 
-    static double CalculateTGasLayerAT(double compWidth, double compLength, double compHeight, double ao)
-    {
-        return ((2*(compWidth*compLength) + 2*(compHeight*compWidth) + 2*(compHeight*compLength)) - ao);
-    }
-
+    static double CalculateTGasLayerAT(double compWidth, double compLength, double compHeight, double ao) { return ((2*(compWidth*compLength) + 2*(compHeight*compWidth) + 2*(compHeight*compLength)) - ao); }
 
     //This method calculates the Value of Q according to the Method of McCaffrey, Quintiera, and Harkleroad in kW
-    static double CalculateMccaffreyQ(double hk, double at, double av, double ventHeight)
-    {
-        return (610 * Math.sqrt(hk*at*av*Math.sqrt(ventHeight)));
-    }
+    static double CalculateMccaffreyQ(double hk, double at, double av, double ventHeight) { return (610 * Math.sqrt(hk*at*av*Math.sqrt(ventHeight))); }
+
     //This method calculates the value of Q according to the method of Babrauskas
-    static double CalculateBarbraukas(double av, double ventHeight)
-    {
-        return (750 * av * Math.sqrt(ventHeight));
-    }
+    static double CalculateBarbraukas(double av, double ventHeight) { return (750 * av * Math.sqrt(ventHeight)); }
+
     //This method calculates the value of Q according to the method of Thomas
-    static double CalculateThomas(double av, double at, double ventHeight)
-    {
-        return (7.8 * at + 378 * av * Math.sqrt(ventHeight));
-    }
+    static double CalculateThomas(double av, double at, double ventHeight) { return (7.8 * at + 378 * av * Math.sqrt(ventHeight)); }
+
     //This method converts the Q calculation from kW to Btu/sec
     static double CalculateBtuPerSec(double kw)
     {
@@ -58,49 +42,23 @@ class Calculations
         return (mbf*ehc*area);
     }
 
-    static double CalculateDiameterFromArea(double diameter)
-    {
-        return (2 * Math.sqrt(diameter/Math.PI));
-    }
+    static double CalculateDiameterFromArea(double diameter) { return (2 * Math.sqrt(diameter/Math.PI)); }
 
-    static double CalculateDiameterFromLengthWidth(double length, double width)
-    {
-        return Math.sqrt(4*((length) * width) / Math.PI);
-    }
+    static double CalculateDiameterFromLengthWidth(double length, double width) { return Math.sqrt(4*((length) * width) / Math.PI); }
 
-    static double CalculateFlameHeight(double q, double diameter)
-    {
-        return (0.23 * Math.pow(q,0.4)-1.02*diameter);
-    }
+    static double CalculateFlameHeight(double q, double diameter) { return (0.23 * Math.pow(q,0.4)-1.02*diameter); }
 
-    static double CalculateHeatFluxtoTarget(double l, double d)
-    {
-        return (15.4 * Math.pow((l/d),-1.59));
-    }
-    static double CalculateOpenPipeQ(double dP, double d, double l, double sg)
-    {
-        return (0.00403*Math.pow(dP*(Math.pow(d,4.8)/(Math.pow(sg,0.8)*l)),0.555));
-    }
+    static double CalculateHeatFluxtoTarget(double l, double d) { return (15.4 * Math.pow((l/d),-1.59)); }
 
-    static double CalculateConductiveHeatFlux(double length, double thermalConductivity, double hotSideTemp, double coldSideTemp)
-    {
-        return (thermalConductivity * (hotSideTemp-coldSideTemp)/length);
-    }
+    static double CalculateOpenPipeQ(double dP, double d, double l, double sg) { return (0.00403*Math.pow(dP*(Math.pow(d,4.8)/(Math.pow(sg,0.8)*l)),0.555)); }
 
-    static double CalculateConductiveThermalPenTime(double length, double thermalDiffucivity)
-    {
-        return (Math.pow(length,2)/(16*thermalDiffucivity));
-    }
+    static double CalculateConductiveHeatFlux(double length, double thermalConductivity, double hotSideTemp, double coldSideTemp) { return (thermalConductivity * (hotSideTemp-coldSideTemp)/length); }
 
-    static double CalculateThermallyThinTimeToIgnition(double density, double specificHeat, double thickness, double ignitionTemp, double ambientTemp, double heatFlux)
-    {
-        return ((density*specificHeat*thickness*(ignitionTemp-ambientTemp))/heatFlux);
-    }
+    static double CalculateConductiveThermalPenTime(double length, double thermalDiffucivity) { return (Math.pow(length,2)/(16*thermalDiffucivity)); }
 
-    static double CalculateThermallyThickTimeToIgnition(double c, double density, double specificHeat, double thermalConductivity, double ignitionTemp, double ambientTemp, double heatFlux)
-    {
-        return (c*thermalConductivity*density*specificHeat*(Math.pow((ignitionTemp-ambientTemp)/heatFlux,2)));
-    }
+    static double CalculateThermallyThinTimeToIgnition(double density, double specificHeat, double thickness, double ignitionTemp, double ambientTemp, double heatFlux) { return ((density*specificHeat*thickness*(ignitionTemp-ambientTemp))/heatFlux); }
+
+    static double CalculateThermallyThickTimeToIgnition(double c, double density, double specificHeat, double thermalConductivity, double ignitionTemp, double ambientTemp, double heatFlux) { return (c*thermalConductivity*density*specificHeat*(Math.pow((ignitionTemp-ambientTemp)/heatFlux,2))); }
 
     static double CalculateThermallyThickTimeToIgnitionWithMaterialSelected(double thermalinertia, double ignitionTemp, double criticalIgnitionFlux, double c, double ambientTemp, double heatFluxExposure)
     {
